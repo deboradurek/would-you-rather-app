216aa6e617000e440752c1f6f906e2e0c382dafc
Debora Durek <debora.durek@gmail.com>
2021-06-07 16:17:15 -0500

    feat: Remove typography component

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -1,15 +1,7 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 
 export default function TabPanel(props) {
   const { children, value, index } = props;
 
-  return (
-    <div>
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+  return value === index && <Box p={3}>{children}</Box>;
 }
