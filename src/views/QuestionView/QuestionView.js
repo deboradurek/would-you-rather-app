dc42ff18d9b5f4bb700e6b42689349c8cea3b476
Debora Durek <debora.durek@gmail.com>
2021-06-09 16:07:26 -0500

    feat: Add dynamic info to each question poll

--- src/views/QuestionView/QuestionView.js
+++ src/views/QuestionView/QuestionView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardContent } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 import getQuestions from '../../actions/questions';
 
@@ -10,14 +11,45 @@ class QuestionView extends Component {
   }
 
   render() {
-    const { users, isLoading, questions } = this.props;
+    const { questionId, users, isLoading, questions } = this.props;
 
-    console.log(this.props);
+    console.log('Props: ', this.props);
 
     return (
       <>
         {!isLoading ? (
-          <p>QuestionView</p>
+          <Card variant="outlined" style={{ marginTop: 50 }}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                {users[questions[questionId].author].name} asks:
+              </Typography>
+              <div>
+                <img
+                  src={users[questions[questionId].author].avatarURL}
+                  alt={users.name}
+                  style={{ height: 150, width: 150, marginTop: 40 }}
+                ></img>
+              </div>
+
+              <Typography variant="h5" component="h2">
+                Would you rather
+              </Typography>
+              <br />
+              <div>
+                <Typography color="textSecondary" variant="body2" component="p">
+                  {questions[questionId].optionOne.text}
+                </Typography>
+                <Typography color="textSecondary" variant="body2" component="p">
+                  {questions[questionId].optionTwo.text}
+                </Typography>
+              </div>
+            </CardContent>
+            <CardActions>
+              {/* <Link to={`/questions/${question.id}`}> */}
+              <Button size="small">Vote</Button>
+              {/* </Link> */}
+            </CardActions>
+          </Card>
         ) : (
           <Card variant="outlined">
             <CardContent>
@@ -30,8 +62,11 @@ class QuestionView extends Component {
   }
 }
 
-function mapStateToProps({ users, questions: { isLoading, questions } }) {
+function mapStateToProps({ users, questions: { isLoading, questions } }, props) {
+  const { question_id } = props.match.params;
+
   return {
+    questionId: question_id,
     users,
     isLoading,
     questions,
