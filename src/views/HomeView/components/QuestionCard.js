62b00906308383d3ed3fc319b823493fa7cee7a1
Debora Durek <debora.durek@gmail.com>
2021-06-09 14:06:00 -0500

    feat: Add users info to connected component

--- src/views/HomeView/components/QuestionCard.js
+++ src/views/HomeView/components/QuestionCard.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import { connect } from 'react-redux';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-export default function QuestionCard() {
-  return (
-    <Card variant="outlined">
-      <div>avatar</div>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Olsen George asks:
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Would you rather
-        </Typography>
-        <br />
-        <Typography color="textSecondary" variant="body2" component="p">
-          ... be a dog ...
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View Pull</Button>
-      </CardActions>
-    </Card>
-  );
+class QuestionCard extends Component {
+  render() {
+    const { users, question } = this.props;
+
+    return (
+      <Card variant="outlined">
+        <div>
+          <img
+            src={users[question.author].avatarURL}
+            alt={users.name}
+            style={{ height: 100, width: 100, marginRight: 12 }}
+          ></img>
+        </div>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            {users[question.author].name} asks:
+          </Typography>
+          <Typography variant="h5" component="h2">
+            Would you rather
+          </Typography>
+          <br />
+          <Typography color="textSecondary" variant="body2" component="p">
+            ... {question.optionOne.text} ...
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View Pull</Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
+
+function mapStateToProps({ users, questions: { questions } }, { questionId }) {
+  const question = questions[questionId];
+  return {
+    users,
+    question,
+  };
+}
+
+export default connect(mapStateToProps)(QuestionCard);
