917bdbfd2eaa343a3223c89e930fbade33e7658b
Debora Durek <debora.durek@gmail.com>
2021-06-09 17:11:01 -0500

    feat: Add initial routes to main app

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,42 @@ import NavBar from './NavBar';
 import HomeView from '../views/HomeView/HomeView';
 import LoginView from '../views/LoginView/LoginView';
 import getUsers from '../actions/users';
-import getQuestions from '../actions/questions';
+import QuestionView from '../views/QuestionView/QuestionView';
+import AddView from '../views/AddView/AddView';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(getUsers());
-    this.props.dispatch(getQuestions());
   }
 
   render() {
+    const { users } = this.props;
+
     return (
       <Router>
         <>
           <NavBar />
-          <Box m={8}>
-            <LoginView />
-          </Box>
-          {/* <Route path="/" exact component={HomeView} /> */}
+          {users && (
+            // {/* <Box m={8}>
+            //   <LoginView />
+            // </Box> */}
+            <>
+              <Route path="/" exact component={HomeView} />
+              <Route path="/questions/:question_id" component={QuestionView} />
+              <Route path="/add" component={AddView} />
+            </>
+            // <QuestionView />
+          )}
         </>
       </Router>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ users }) {
+  return {
+    users,
+  };
+}
+
+export default connect(mapStateToProps)(App);
