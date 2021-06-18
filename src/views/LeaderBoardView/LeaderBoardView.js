5da941d5b50fc6c65acbcdd8d58d0e0da75e4ce8
Debora Durek <debora.durek@gmail.com>
2021-06-17 22:01:17 -0500

    feat: Add styled components and transform to func

--- src/views/LeaderBoardView/LeaderBoardView.js
+++ src/views/LeaderBoardView/LeaderBoardView.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
-import { Typography } from '@material-ui/core';
+import React from 'react';
+import { Box } from '@material-ui/core';
 import LeaderCard from './components/LeaderCard';
 
-class LeaderBoardView extends Component {
-  render() {
-    return (
-      <div>
-        <Typography variant="h5" component="h2">
-          LeaderBoard{' '}
-        </Typography>
-        <LeaderCard />
-      </div>
-    );
-  }
+export default function LeaderBoardView() {
+  return (
+    <Box paddingTop={3}>
+      <LeaderCard />
+    </Box>
+  );
 }
-
-export default LeaderBoardView;
